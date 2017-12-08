f3c68375ed10affe4580157458d01f376c67429f
Eumir <eumirollarvez@gmail.com>
2017-12-07 21:43:23 -0300

    Carga dinamica about

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { InformacionService } from "../../services/informacion.service"
 
 @Component({
   selector: 'app-about',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
 })
 
 export class AboutComponent {
+
+  constructor(public _is:InformacionService){
+    
+  }
 
 }
