407a749465176a6e400c66a4bf593027c2266510
Eumir <eumirollarvez@gmail.com>
2017-12-07 21:19:06 -0300

    Servicio de firebase listo

--- src/app/services/informacion.service.ts
+++ src/app/services/informacion.service.ts
@@ -6,16 +6,30 @@ export class InformacionService {
 
   info:any = {};
   cargada:boolean = false;
+  equipo:any[] = [];
+  cargada_conocenos:boolean = false;
 
   constructor(public http:Http) {
+    this.carga_info();
+    this.carga_conocenos();
+  }
 
+  public carga_info(){
     this.http.get("assets/data/info.pagina.json")
               .subscribe(data => {
-                console.log(data.json());
+                //console.log(data.json());
                 this.cargada = true;
                 this.info = data.json();
               });
+  }
 
+  public carga_conocenos(){
+    this.http.get("https://pilotos-a97b4.firebaseio.com/equipo.json")
+              .subscribe(data => {
+                console.log(data.json());
+                this.cargada_conocenos = true;
+                this.equipo = data.json();
+              });
   }
 
 }
